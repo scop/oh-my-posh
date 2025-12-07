f416ed98a8c5e0d599d821f62218e869172273a9
Ville Skyttä <ville.skytta@iki.fi>
2025-12-07 11:12:58 +0200

    fix(bash): skip also source commands done with "." on hook install

--- src/shell/scripts/omp.bash
+++ src/shell/scripts/omp.bash
@@ -151,7 +151,7 @@ function _omp_install_hook() {
 
         # check if the command starts with source, if so, do not add it again
         # this is done to avoid issues with sourcing the same file multiple times
-        if [[ $cmd == "source "* ]]; then
+        if [[ $cmd == "source "* || $cmd == ". "* ]]; then
             continue
         fi
 
